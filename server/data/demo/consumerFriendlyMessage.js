745df9eda997ec57494688e97670ce19de9e21b9
Zhou Xiao <randy.zhou@gmail.com>
2015-01-19 17:37:37 +0800

    Move demo folder under folder data

---
+++ server/data/demo/consumerFriendlyMessage.js
@@ -0,0 +1,42 @@
+[
+	{
+		"_id": "63f75cfb891ff700002d3500",
+		"name": "Customer Friendly Message Tester",
+		"description": "The data is for test purpose of Cusotmer Friendly Message",
+		"version": "001.001.015",
+		"messages": [
+					  	{
+							"description": "Application is unauthorized",
+							"languages": [
+													{
+														"enabled": true,
+														"tts": "This version of appName is not authorized and will not work with SDL.",
+														"lines": [
+																		{
+																			"line1": "Not Authorized"
+																		}
+																	]
+													}
+												]
+							}, {
+							"description": "Data Consent",
+							"languages": [
+													{
+														"enabled": true,
+														"tts": "To use mobile apps with SDL, SDL may use your mobile devices's data plan... ",
+														"lines": [
+																		{
+																			"line1": "Enable Mobile Apps",
+																			"line2": "on SDL? (Uses Data)"
+																		}
+																	]
+													}
+												]
+							}	
+						  ]
+	},
+	{
+		"_id": "63f75cfb891ff700002d3502",
+		"name": "Default for Empty"
+	}	
+]
